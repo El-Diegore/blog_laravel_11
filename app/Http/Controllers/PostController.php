f539c45c124e3efe1550e9ffa76af72954c3961c
Diego <pinto.p.diego@gmail.com>
2024-08-09 12:54:34 -0400

    se agrega posts model, views

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,20 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index(){
         
-            $posts = [
-                ['title' => 'First post'],
-                ['title' => 'Second post'],
-                ['title' => 'Third post'],
-                ['title' => 'Fourth post']
-            ];
-         return view('blog', ['posts'=>$posts]
+            $posts = Post::get();
+         return view('posts.index', ['posts'=>$posts]
         );  
         
+    }
+    public function show(Post $post){
+        return view('posts.show', ['post'=> $post]);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,10 @@
 <x-layout meta-title="Blog title" meta-description="Blog description">
     <h1>Blog</h1>
     @foreach ($posts as $post)
-        {{$post['title'] }}
+    <h2>
+        <a href="{{route('posts.show', $post) }}">{{$post->title }}</a>
+    </h2>
+        
     @endforeach
 </x-layout>
 
