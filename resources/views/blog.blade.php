d6b4299c9874a76b25bc336d3d745bf8ec50dee4
Diego <pinto.p.diego@gmail.com>
2024-08-09 11:42:58 -0400

    creación PostController

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,6 +1,8 @@
 {{--   @component('components.layout') es o mism que x layout--}}    
 <x-layout meta-title="Blog title" meta-description="Blog description">
     <h1>Blog</h1>
-    
+    @foreach ($posts as $post)
+        {{$post['title'] }}
+    @endforeach
 </x-layout>
 
